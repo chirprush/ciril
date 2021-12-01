30e386e30e7b9467b18f7c67ec57cecd1410b6f9
chirprush <chirpyboat73@gmail.com>
2021-11-30 20:21:53 -0500

    Removed argument parsing
    I'm not really happy with how the argument parsing is implemented and
    I feel that the best thing to do is remove it and implement it when
    the program actually does something

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,6 @@
 module Main where
 
 import System.Environment
-import Data.Maybe
-import ArgParse
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case getArgsOptions ArgOptions { optUsage = Nothing, optFiles = [] } args of
-      Left options -> if (isJust $ optUsage options) && (fromJust $ optUsage options) then
-                        putStrLn argsUsage
-                      else print options
-      Right error -> putStrLn error
+main = putStrLn "Hello, World!"
